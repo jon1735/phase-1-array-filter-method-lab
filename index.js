4504d3d443d897ba10b0b52eb759fe57dadb1c71
Jonathan Gonzalez <Jonathan.gonzalez@powerhrg.com>
2021-05-26 08:35:59 -0400

    adding updated index.js file

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(drivers, name){
+    const newDriversArray = drivers.filter(string => string.toLowerCase() === name.toLowerCase())
+    return newDriversArray
+}
+
+function fuzzyMatch(drivers, name) {
+    const newDriversArray = drivers.filter(string => string.charAt(0) === name.charAt(0))
+    return newDriversArray
+}
+
+function matchName (drivers, name){
+    const newDriversArray = drivers.filter(drivers => drivers.name === name)
+    return newDriversArray
+}
